aae093ef1e0d702f6fbb87acf82fb03dc1a1d220
Yusinto Ngadiman <yusinto@gmail.com>
2017-11-30 21:12:36 +1100

    Added more tests for queryMatchMiddleware
    Added more tests for queryMatchMiddleware

--- src/exports/matchQueryMiddleware.test.js
+++ src/exports/matchQueryMiddleware.test.js
@@ -1,22 +1,33 @@
-jest.mock('body-parser', () => ({json: global.td.function('mockBodyParser.json')}));
-
-import bodyParser from 'body-parser';
 
 describe('matchQueryMiddleware', () => {
+  const originalConsoleLog = console.log;
+  console.log = td.function('mockConsoleLog');
+
+  const mockRequest = {body: {queryId: 'animalMd5'}};
+  const mockQueryMapJson = {animalMd5: 'query { animal }'};
+  const mockJsonParser = td.function('mockJsonParser');
+
+  const mockNext = td.function('mockNext');
+  let matchQueryMiddleware;
+  let captor;
+
+  beforeEach(() => {
+    jest.doMock('body-parser', () => ({json: () => mockJsonParser}));
+    captor = td.matchers.captor();
+    matchQueryMiddleware = require('./matchQueryMiddleware').default;
+  });
+
   afterEach(() => {
     td.reset();
     jest.resetAllMocks();
+    jest.resetModules();
+  });
+
+  afterAll(() => {
+    console.log = originalConsoleLog;
   });
 
   test('should map queryId to actual query', () => {
-    const mockJsonParser = td.function('mockJsonParser');
-    td.when(bodyParser.json()).thenReturn(mockJsonParser);
-    const mockNext = td.function('mockNext');
-    const mockRequest = {body: {queryId: 'animalMd5'}};
-    const mockQueryMapJson = {animalMd5: 'query { animal }'};
-    const captor = td.matchers.captor();
-
-    const matchQueryMiddleware = require('./matchQueryMiddleware').default;
     const middleware = matchQueryMiddleware(mockQueryMapJson);
     middleware(mockRequest, null, mockNext);
 
@@ -26,28 +37,29 @@ describe('matchQueryMiddleware', () => {
 
     expect(mockRequest.body.query).toEqual('query { animal }');
     td.verify(mockNext());
+    td.verify(console.log(td.matchers.anything()), {times: 0});
   });
 
-  // TODO:
-  // test('should map queryId to actual query', () => {
-  //   console.log = td.function();
-  //   const mockJsonParser = td.function('mockJsonParser');
-  //   td.when(bodyParser.json()).thenReturn(mockJsonParser);
-  //   const mockNext = td.function('mockNext');
-  //   const mockRequest = {body: {queryId: 'animalMd5'}};
-  //   const mockQueryMapJson = {animalMd5: 'query { animal }'};
-  //   const captor = td.matchers.captor();
-  //
-  //   const matchQueryMiddleware = require('./matchQueryMiddleware').default;
-  //   const middleware = matchQueryMiddleware(mockQueryMapJson, true);
-  //   middleware(mockRequest, null, mockNext);
-  //
-  //   td.verify(mockJsonParser(td.matchers.anything(), td.matchers.anything(), captor.capture()));
-  //   const queryMapFunction = captor.values[0];
-  //   queryMapFunction();
-  //
-  //   expect(mockRequest.body.query).toEqual('query { animal }');
-  //   td.verify(mockNext());
-  //   td.verify(console.log(td.matchers.anything()), {times: 2});
-  // });
+  test('should log when debug is true', () => {
+    const middleware = matchQueryMiddleware(mockQueryMapJson, true);
+    middleware(mockRequest, null, mockNext);
+
+    td.verify(mockJsonParser(td.matchers.anything(), td.matchers.anything(), captor.capture()));
+    const queryMapFunction = captor.values[0];
+    queryMapFunction();
+
+    expect(mockRequest.body.query).toEqual('query { animal }');
+    td.verify(mockNext());
+    td.verify(console.log(td.matchers.anything()), {times: 2});
+  });
+
+  test('should throw error when queryId is not found', () => {
+    const middleware = matchQueryMiddleware(mockQueryMapJson, true);
+    mockRequest.body.queryId = 'does-not-exist';
+    middleware(mockRequest, null, mockNext);
+
+    td.verify(mockJsonParser(td.matchers.anything(), td.matchers.anything(), captor.capture()));
+    const queryMapFunction = captor.values[0];
+    expect(queryMapFunction).toThrow('matchQueryMiddleware: can\'t find queryId: does-not-exist');
+  });
 });

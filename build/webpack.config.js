2e7729b5b3ccf2b2395539e5f8b3bdcf5f1c2720
Yusinto Ngadiman <yusinto@gmail.com>
2018-03-07 20:40:18 +1100

    a

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -13,7 +13,7 @@ export default {
   },
   externals: [nodeExternals(), 'relay-compiler-plus/graphqlJSCompiler'], // ignore all modules in node_modules folder
   output: {
-    path: path.resolve('./dist/bin'),
+    path: path.resolve('./bin'),
     filename: 'relay-compiler-plus',
     pathinfo: true,
     library: 'RelayCompilerPlus',
